0bf627265479cb1f4198d645a1b5af6b3f319a68
patelvishal937 <patelvishal93137@gmail.com>
2023-11-08 11:52:32 +0530

    appointment showcase in time duration date(syncfusion)

--- lib/SyncfusionCalanderWIdget.dart
+++ lib/SyncfusionCalanderWIdget.dart
@@ -9,6 +9,15 @@ class SyncFusionCalander extends StatefulWidget {
 }
 
 class _SyncFusionCalanderState extends State<SyncFusionCalander> {
+  CalendarController _calendarController = CalendarController();
+  late CalendarDataSource _dataSource;
+
+  @override
+  initState() {
+    _dataSource = _getCalendarDataSource();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,10 +33,32 @@ class _SyncFusionCalanderState extends State<SyncFusionCalander> {
                   height: 2 * MediaQuery.of(context).size.height / 3,
 
                   // sfCalendar use for creating custom calander
-                  
+
                   child: SfCalendar(
                     // showcase which type of calander shown in UI
+
                     view: CalendarView.month,
+                    // dataSource
+
+                    dataSource: _getCalendarDataSource(),
+                    // onviechanged for displaying agenda
+
+                    onViewChanged: (ViewChangedDetails details) {
+                      List<DateTime> dates = details.visibleDates;
+                      String calenderTimezone = '';
+                      List<Object> appointment =
+                          _dataSource.getVisibleAppointments(
+                              dates[0],
+                              calenderTimezone,
+                              dates[(details.visibleDates.length) - 1]);
+                    },
+                    // This is for showing appointment for each date
+
+                    monthViewSettings: const MonthViewSettings(
+                      appointmentDisplayCount: 1,
+                      appointmentDisplayMode:
+                          MonthAppointmentDisplayMode.appointment,
+                    ),
 
                     // apply color for selected date
                     selectionDecoration: BoxDecoration(
@@ -45,4 +76,135 @@ class _SyncFusionCalanderState extends State<SyncFusionCalander> {
       ),
     );
   }
+}
+
+// Create the Meeting Datasource
+MeetingDataSource _getCalendarDataSource() {
+  List<Meeting> meetings = <Meeting>[];
+  // This is for start date
+  final DateTime today = DateTime.now();
+  final DateTime startTime = DateTime(today.year, today.month, today.hour);
+  final DateTime endTime = startTime.add(Duration(hours: 1));
+  meetings.add(Meeting(
+      eventName: "hello",
+      from: startTime,
+      to: endTime,
+      background: Colors.green,
+      startTimeZone: '',
+      endTimeZone: '',
+      isAllDay: true));
+  meetings.add(
+    Meeting(
+        eventName: "hello",
+        from: DateTime(2023, 11, 1, 9),
+        to: DateTime(2023, 11, 1, 11),
+        background: Colors.green,
+        startTimeZone: '',
+        endTimeZone: '',
+        isAllDay: true),
+  );
+  meetings.add(
+    Meeting(
+        eventName: "hello",
+        from: DateTime(2023, 11, 1, 9),
+        to: DateTime(2023, 11, 1, 11),
+        background: Colors.green,
+        startTimeZone: '',
+        endTimeZone: '',
+        isAllDay: true),
+  );
+  meetings.add(
+    Meeting(
+        eventName: "hello",
+        from: DateTime(2023, 11, 2, 9),
+        to: DateTime(2023, 11, 2, 11),
+        background: Colors.green,
+        startTimeZone: '',
+        endTimeZone: '',
+        isAllDay: true),
+  );
+  meetings.add(
+    Meeting(
+        eventName: "hello",
+        from: DateTime(2023, 11, 2, 9),
+        to: DateTime(2023, 11, 2, 11),
+        background: Colors.green,
+        startTimeZone: '',
+        endTimeZone: '',
+        isAllDay: true),
+  );
+  meetings.add(
+    Meeting(
+        eventName: "hello",
+        from: DateTime(2023, 11, 3, 9),
+        to: DateTime(2023, 11, 3, 11),
+        background: Colors.green,
+        startTimeZone: '',
+        endTimeZone: '',
+        isAllDay: true),
+  );
+
+  return MeetingDataSource(meetings);
+}
+
+//  map properties of Meeting class with our calendar widget by using the CalendarDataSource override methods properties.
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from as DateTime;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to as DateTime;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  String getStartTimeZone(int index) {
+    return appointments![index].startTimeZone;
+  }
+
+  @override
+  String getEndTimeZone(int index) {
+    return appointments![index].endTimeZone;
+  }
+}
+
+// Create a Custom Meeting
+class Meeting {
+  Meeting(
+      {this.endTimeZone = "",
+      this.startTimeZone = "",
+      required this.eventName,
+      required this.from,
+      required this.to,
+      required this.background,
+      required this.isAllDay});
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+  String startTimeZone;
+  String endTimeZone;
 }
