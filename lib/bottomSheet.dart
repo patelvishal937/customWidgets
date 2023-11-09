8aceec139d9eba56587d2e6ea95081ead9cbe31c
patelvishal937 <patelvishal93137@gmail.com>
2023-11-10 00:31:25 +0530

    working on Listveiw

--- lib/bottomSheet.dart
+++ lib/bottomSheet.dart
@@ -9,6 +9,13 @@ class BottomBarCustomWidget extends StatefulWidget {
 
 class _BottomBarCustomWidgetState extends State<BottomBarCustomWidget> {
   @override
+  List<List<String>> listsData = [
+    ['Item 1', 'Item 2', 'Item 3'],
+    ['Item A', 'Item B', 'Item C', 'Item D'],
+    ['Item X', 'Item Y', 'Item Z'],
+    ['Item P', 'Item Q', 'Item R'],
+    ['Item M', 'Item N', 'Item O'],
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -17,13 +24,70 @@ class _BottomBarCustomWidgetState extends State<BottomBarCustomWidget> {
       body: GestureDetector(
         onTap: () {
           showModalBottomSheet(
+              backgroundColor: Color.fromARGB(255, 9, 6, 6),
               context: context,
               builder: (BuildContext context) {
-                return Text("hello");
+                return Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      margin: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 4, horizontal: 4),
+                            child: Icon(Icons.search),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 4, horizontal: 16),
+                            child: Text("Search"),
+                          ),
+                          // Padding(
+                          //   padding: const EdgeInsets.symmetric(
+                          //       vertical: 4, horizontal: 6),
+                          //   child: Container(
+                          //       // width: double.infinity,
+                          //       child: Icon(Icons.people)),
+                          // ),
+                        ],
+                      ),
+                    ),
+                    // ListView.builder(
+                    //   itemCount: List,
+                    //   itemBuilder: (BuildContext context, int index) {
+                    //     return ListTile(
+                    //       title: Text('h'),
+                    //     );
+                    //   },
+                    // )
+                  ],
+                );
               });
         },
-        child: Container(
-          child: Center(child: const Text("Hello")),
+        // This is customButton for open The bottomSheet
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.all(12),
+            height: MediaQuery.of(context).size.height / 12,
+            decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(10)),
+            child: const Center(
+              child: Text(
+                "Open The BottomSheet",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ),
         ),
       ),
     );
