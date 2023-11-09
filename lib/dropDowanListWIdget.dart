6b626462aad47e0d8ca76ce2d9ef38fefa8b348e
patelvishal937 <patelvishal93137@gmail.com>
2023-11-09 23:36:38 +0530

    dropDownWidget

--- lib/dropDowanListWIdget.dart
+++ lib/dropDowanListWIdget.dart
@@ -8,39 +8,56 @@ class DropdownList extends StatefulWidget {
 }
 
 class _DropdownListState extends State<DropdownList> {
-  String? dropdownValue;
-  List<String> items = ["ohhkjhks", "two", "three"];
+  String? DropDownValue;
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("appbar"),
       ),
-      body: DropdownButton<String>(
-        value: dropdownValue,
-        icon: Icon(Icons.arrow_drop_down_circle),
-        iconSize: 30,
-        elevation: 16,
-        style: TextStyle(color: Colors.teal),
-        underline: Container(
-          height: 2,
-          color: Colors.black,
+      body: Center(
+        child: Card(
+          elevation: 2,
+          color: Colors.blueAccent,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                  // This value is for current value of dropdown list
+                  value: DropDownValue,
+                  // showcase when no Items are selected
+                  hint: Text('Items'),
+                  // color for Dropdown
+                  dropdownColor: Colors.blueAccent,
+                  // get the data from List showcase items
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(
+                        items,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    );
+                  }).toList(),
+                  // assign new value to dropdown value
+                  onChanged: (String? value) {
+                    setState(() {
+                      DropDownValue = value!;
+                    });
+                  }),
+            ),
+          ),
         ),
-        hint: Text("enter the subject"),
-        onChanged: (newvalue) {
-          setState(
-            () {
-              dropdownValue = newvalue.toString();
-            },
-          );
-        },
-        items: <String>['hell', 'Two', 'Three', 'Four']
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
       ),
     );
   }
